5f4bba27b7082d087abd758e5813784642f91f94
Minank Mehta <112801417+MinankMehta@users.noreply.github.com>
2022-12-01 01:08:26 +0530

    changes in php

--- connect.php
+++ connect.php
@@ -1,21 +1,21 @@
 <?php
-    $firstName= $_POST['firstName'];
-    $lastName= $_POST['lastName'];
-    $emailId= $_POST['emailId'];
-    $mobileNumber= $_POST['mobileNumber'];
-    $messageSend= $_POST['messageSend'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $emailId = $_POST['emailId'];
+    $mobileNumber = $_POST['mobileNumber'];
+    $messageSend = $_POST['messageSend'];
 
     //database connection
-    $conn = new mysqli('localhost','root','','test');
+    $conn = new mysqli('localhost','root','','test1');
     if ($conn->connect_error){
         die('Connection Failed : '.$conn->connect_error);
     }
     else{
         $stmt = $conn->prepare("insert into registration(firstName, lastName, emailId, mobileNumber, messageSend) values(?,?,?,?,?)");
-        $stmt->bind_param("sssis",$firstName,$lastName,$emailId,$mobileNumber,$messageSend);
+        $stmt->bind_param("sssis",$firstName, $lastName, $emailId, $mobileNumber, $messageSend);
         $stmt->execute();
         echo "message sent....";
-        $stmt->close;
-        $conn->close;
+        $stmt->close();
+        $conn->close();
     }
 ?>
