0287314093d85248ee6aed1165985c7482664a34
Sanika Desai <sanikadesai17@gmail.com>
2023-02-14 20:38:12 +0530

    final

--- 3feb full website/script.js
+++ 3feb full website/script.js
@@ -1,30 +1,3 @@
-
-// *********************
-// This Code is for only the floating card in right bottom corner
-// **********************
-
-
-
-// const WebCifarIcon = document.querySelector("#webCifar-icon");
-// const WebCifarEl = document.querySelector("#webCifar");
-// const close = WebCifarEl.querySelector(".close");
-// const youtubeLink = document.querySelector(".youtubeLink");
-
-// WebCifarIcon.addEventListener("click", () => {
-//   WebCifarEl.classList.add("active");
-// });
-// close.addEventListener("click", () => {
-//   WebCifarEl.classList.remove("active");
-// });
-
-// youtubeLink.setAttribute("href", "https://youtu.be/_HTL0XSMcRQ");
-
-
-
-
-
-
-
 // Add active class to the current button (highlight it)
 let prevButton = document.getElementById("pl");
 
